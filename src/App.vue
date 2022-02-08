aae5cbed9e1b057c677a6cc95b82d6f8cc3d5897
xuzhaobin <870110664.com>
2022-02-08 22:49:47 +0800

    husky功能测试

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,8 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-331
-  <img alt="Vue logo" src="./assets/logo.png"        />
+  331
+  <img alt="Vue logo" src="./assets/logo.png" />
   123
   <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
 </template>
