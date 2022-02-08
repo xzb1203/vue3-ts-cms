c5593db91970f8c7d45086b2b908da60b8d6b7c6
xuzhaobin <870110664.com>
2022-02-08 23:26:58 +0800

    feat(config): 配置

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,9 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  33
   <img alt="Vue logo" src="./assets/logo.png" />
-  123
   <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
 </template>
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
-// https://vitejs.dev/config/
+import path from 'path'
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+
+  base: './', // 类似publicPath，'./'避免打包访问后空白页面，要加上，不然线上也访问不了
+  resolve: {
+    alias: {
+      // 如果报错__dirname找不到，需要安装node,执行npm install @types/node --save-dev
+      '@': path.resolve(__dirname, 'src'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@images': path.resolve(__dirname, 'src/assets/images'),
+      '@views': path.resolve(__dirname, 'src/views'),
+      '@store': path.resolve(__dirname, 'src/store')
+    }
+  }
 })
