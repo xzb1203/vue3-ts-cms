3ad18a4db03b21e7bb5d1e076f4102c4cf570c83
xuzhaobin <870110664.com>
2022-02-16 16:59:54 +0800

    feat(table): 完成table组件封装

---
+++ src/base-ui/table/src/table.vue
@@ -0,0 +1,29 @@
+<template>
+  <el-table :data="listData" style="width: 100%" border>
+    <template v-for="propItem in propList" :key="propItem.prop">
+      <el-table-column v-bind="propItem" align="center">
+        <template #default="scope">
+          <slot :name="propItem.slotName" :row="scope.row">
+            {{ scope.row[propItem.prop] }}
+          </slot>
+        </template>
+      </el-table-column>
+    </template>
+  </el-table>
+</template>
+
+<script setup lang="ts">
+import { PropType } from 'vue'
+const props = defineProps({
+  listData: {
+    type: Array,
+    require: true
+  },
+  propList: {
+    type: Array as PropType<any>,
+    require: true
+  }
+})
+</script>
+
+<style scoped lang="scss"></style>
--- src/views/main/system/user/user.vue
+++ src/views/main/system/user/user.vue
@@ -1,22 +1,47 @@
 <template>
   <div class="user">
     <page-search :searchFormConfig="searchFormConfig" />
+    <div class="mt-2rem">
+      <hy-table :listData="userList" :propList="propList">
+        <template #status="scope">
+          <el-tag v-if="scope.row.enable">启用</el-tag>
+          <el-tag v-else>禁用</el-tag>
+        </template>
+      </hy-table>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { searchFormConfig } from './config/search.config'
 import PageSearch from '@/components/page-search'
+import HyTable from '@/base-ui/table'
+import { searchFormConfig } from './config/search.config'
 import { useSystemStore } from '@/store'
 const { getUserList } = useSystemStore()
-onMounted(() => {
-  getUserList({
-    pageUrl: '/users/list',
-    queryInfo: {
-      offset: 0,
-      size: 10
-    }
-  })
+getUserList({
+  pageUrl: '/users/list',
+  queryInfo: {
+    offset: 0,
+    size: 10
+  }
 })
-console.log(useSystemStore().userCount)
+const userList = computed(() => useSystemStore().userList)
+const propList = [
+  { prop: 'name', label: '用户名', minWidth: '100' },
+  { prop: 'realname', label: '真实姓名', minWidth: '100' },
+  { prop: 'cellphone', label: '手机号码', minWidth: '100' },
+  { prop: 'enable', label: '状态', minWidth: '100', slotName: 'status' },
+  {
+    prop: 'createAt',
+    label: '创建时间',
+    minWidth: '250',
+    slotName: 'createAt'
+  },
+  {
+    prop: 'updateAt',
+    label: '更新时间',
+    minWidth: '250',
+    slotName: 'updateAt'
+  }
+]
 </script>
